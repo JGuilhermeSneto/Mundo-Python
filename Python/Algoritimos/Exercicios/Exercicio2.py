09d498ab383730c5bce8ba9f1c8d449712d98ee8
José Guilherme da S. Neto <165957761+JGuilhermeSneto@users.noreply.github.com>
2025-09-23 18:09:33 -0300

    ..

--- Python/Algoritimos/Exercicios/Exercicio2.py
+++ Python/Algoritimos/Exercicios/Exercicio2.py
@@ -1,4 +1,11 @@
-n1 = int(input("Digite o primeiro número:"))
-n2 = int(input("Digite o segundo número:"))
-s = n1 + n2
-print("A soma é:", n1, "+", n2, "=", s)
+try:
+    n1 = float(input("Digite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+    s = n1 + n2
+    if s > 20:
+        print(False, "maior que 20")
+    else:
+        print("Soma dos valores é:", s)
+        print(True)
+except ValueError:
+    print("Por favor, digite apenas números válidos.")
